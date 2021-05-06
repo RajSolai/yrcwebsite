02253723b92282c4cf103aba065d1cb037b49777
RajSolai <msraj085@gmail.com>
2021-05-06 11:22:45 +0530

    added github workflow for deployment

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,1 +1,5 @@
-firebase deploy --token 1//0gRPRuKsUYGh9CgYIARAAGBASNwF-L9IrrTFwU3XIiX8b-9QJ3xtVMihIvNASDPA_vdhmFNnorgETE9jSBsYJK2TLj3hXzFkKRqs --only hosting:srmvecyrc
+firebase_token=1//0gRPRuKsUYGh9CgYIARAAGBASNwF-L9IrrTFwU3XIiX8b-9QJ3xtVMihIvNASDPA_vdhmFNnorgETE9jSBsYJK2TLj3hXzFkKRqs
+
+yarn 
+yarn build
+firebase deploy --token $firebase_token --only hosting:srmvecyrc
