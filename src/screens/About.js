a0a1dc6f8e79ceff5285b53f3489b104255db097
RajSolai <msraj085@gmail.com>
2021-05-15 08:46:01 +0530

    *

--- src/screens/About.js
+++ src/screens/About.js
@@ -1,7 +1,7 @@
-import React from "react";
+import { Component } from "react";
 import Footer from "../components/footer";
 
-class About extends React.Component {
+class About extends Component {
   render() {
     return (
       <>
